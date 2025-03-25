e145a559cdbe34e2a3d9515b12fa642eb6e16e6d
Manel <mahomarienell@gmail.com>
2025-03-25 12:24:38 +0800

    delete client folder

--- client/index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('http://localhost:5000/getAll')
-    .then(response => response.json())
-    .then(data => loadHTMLTable(data['data']));
-
-});
